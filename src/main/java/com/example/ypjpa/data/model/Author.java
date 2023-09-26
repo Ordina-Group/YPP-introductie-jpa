7682c28956607794c21c2b1d17b77f8274c8d709
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-09-26 14:04:15 +0200

    Added attributes and annotations to entities

--- src/main/java/com/example/ypjpa/data/model/Author.java
+++ src/main/java/com/example/ypjpa/data/model/Author.java
@@ -1,4 +1,33 @@
 package com.example.ypjpa.data.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
 public class Author implements AuthorInterface {
+
+    @Id
+    private UUID id;
+
+    private String firstName;
+    private String lastName;
+    private String initials;
+    private Date dateOfBirth;
+
+    @OneToMany
+    private List<Book> books;
+
+    @OneToOne
+    private InventoryItem inventoryItem;
+
+    @Override
+    public List<Book> getBooks(){
+        return books;
+    }
 }
--- src/main/java/com/example/ypjpa/data/model/Book.java
+++ src/main/java/com/example/ypjpa/data/model/Book.java
@@ -1,10 +1,33 @@
 package com.example.ypjpa.data.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+
+@Entity
 public class Book implements BookInterface {
 
+    @Id
+    private String isbn;
+
+    private String title;
+    private String description;
+    private int rating;
+
+    @ManyToOne
+    private Author author;
+
+    @OneToOne
+    private InventoryItem inventoryItem;
 
     @Override
     public String toString() {
-        return ""; //todo should contain at least title
+        return "title: " + title;
+    }
+
+    @Override
+    public Author getAuthor() {
+        return author;
     }
 }
